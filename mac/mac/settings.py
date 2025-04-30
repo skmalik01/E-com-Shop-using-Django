40b1b0bf86f3c346e0bd7d2e32ef045b8e7651f3
malik01 <www.maliksk@gmail.com>
2025-04-30 19:05:57 +0530

    change

--- mac/mac/settings.py
+++ mac/mac/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import dotenv_values
+config = dotenv_values(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lkw(6*8e+gi@8+-gkacbq56u5q$s8h&&@=84+i_mqsn*%emvm='
+SECRET_KEY = config.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -59,9 +62,8 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # Google OAuth2 credentials
-import os 
-from dotenv import dotenv_values
-config = dotenv_values(".env")
+
+
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
